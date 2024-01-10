993f9d9eb518a4a70fcad5c250fb3d7686fa6b02
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-01-11 02:32:43 +0530

    final landingpage

---
+++ js/mail.js
@@ -0,0 +1,13 @@
+function sendEmail(event){
+  event.preventDefault(); 
+  console.log("clicked");
+  const emailId = document.getElementById("email").value;
+  console.log(`email id is ${emailId}`);
+  Email.send({
+SecureToken : "65c93064-4950-43e9-b620-5a66bf02c8c9",
+To : emailId,
+From : "connect@trivolo.in",
+Subject : "This is the subject",
+Body : "And this is the body"
+})
+}
